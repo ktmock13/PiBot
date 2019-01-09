081eb0a9454b03c6793ead9019bd94d7d7132fcf
ktmock13 <ktmock13@gmail.com>
2019-01-08 21:11:35 -0800

    update

--- arm.py
+++ arm.py
@@ -43,14 +43,12 @@ class LaserArm:
 
     def captureCenter(self):
         self.center = self.duties
-        # print "center %s" % (self.duties)
 
     def setDefaults(self):
         self.reset((self.maxInputs['x'], self.maxInputs['y']))
 
     def captureRange(self, axis):
         self.range[axis] = abs(self.duties[axis] - self.center[axis])
-        # print "range %s" % (self.range[axis])
 
     def motion(self, event):
         self.position(event.x, event.y)
