157093d7b0e85947d4c7df6abb0d239f7a323281
ktmock13 <ktmock13@gmail.com>
2019-01-10 19:34:19 -0800

    update

--- arm.py
+++ arm.py
@@ -39,8 +39,6 @@ class LaserArm:
         print "x %d y %d" % (round(dutyX), round(dutyY))
 
     def resetServos(self):
-        dutyX = ((self.center['x']) - (((x / self.maxInputs['x']) * self.range['x'] * 2) - self.range['x']))
-        dutyY = ((self.center['y']) + (((y / self.maxInputs['y']) * self.range['y'] * 2) - self.range['y']))
         print "maxxing  x %d y %d" % (round(dutyX), round(dutyY))
         self.pwm.set_pwm(CHANNEL_X, 0, 0)
         self.pwm.set_pwm(CHANNEL_Y, 0, 0)
