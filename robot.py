cc698f2067ec85e7b0efd59920cf451fe4dec682
ktmock13 <ktmock13@gmail.com>
2019-01-10 20:40:28 -0800

    update

--- robot.py
+++ robot.py
@@ -67,9 +67,10 @@ class OutputWindow:
         if self.robot.isTracking:
             faces = self.robot.trackingHaar.detectMultiScale(frame, 1.1, 5)
             for (x,y,w,h) in faces:
-                xPercent = float(x)/float(self.robot.eye.camera.resolution[0]);
-                yPercent = float(y)/float(self.robot.eye.camera.resolution[1]);
-                print ('Face at percents...', xPercent, yPercent, 'Size...', w, ' x ', h)
+                xPercent = float(x+(w/2))/float(self.robot.eye.camera.resolution[0]);
+                yPercent = float(y+(h/2))/float(self.robot.eye.camera.resolution[1]);
+                cv2.rectangle(gray,(x,y),(x+w,y+h),(255,255,0),1)
+                print ('Face at percents...', xPercent, yPercent, 'Size...', w, ' x ' h)
                 self.robot.arm.positionPercent(xPercent,yPercent)
         else:
             for guideKey in guides:
