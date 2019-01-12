25d27969ec042a8f39dc2556d24cad0f44f7a212
ktmock13 <ktmock13@gmail.com>
2019-01-11 21:38:49 -0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from robot import Robot
 import signal
 import sys
+import time
 
 if __name__ == '__main__':
 
@@ -9,7 +10,7 @@ if __name__ == '__main__':
     def dont_explode(signal, frame):
         print("NOT EXPLODING, goodbye")
         dick.kill()
-        sleep(3)
+        time.sleep(3)
         sys.exit(0)
 
     signal.signal(signal.SIGINT, dont_explode)
