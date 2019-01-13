77b563fd90c48ab88094be276e24621ed74289c9
ktmock13 <ktmock13@gmail.com>
2019-01-12 22:46:21 -0800

    update

--- arm.py
+++ arm.py
@@ -8,7 +8,7 @@ import Adafruit_PCA9685
 CHANNEL_X = 3
 CHANNEL_Y = 11
 LASER_PIN = 37
-ROUGH_CENTER = (221, 427)
+ROUGH_CENTER = (100, 100)
 
 
 class LaserArm:
@@ -38,8 +38,8 @@ class LaserArm:
         self.currentPercents = { 'x': x / self.maxInputs['x'], 'y': y / self.maxInputs['y'] } 
         dutyX = ((self.center['x']) - (((x / self.maxInputs['x']) * self.range['x'] * 2) - self.range['x'])) +100
         dutyY = ((self.center['y']) + (((y / self.maxInputs['y']) * self.range['y'] * 2) - self.range['y'])) +100
-        self.pwm.set_pwm(CHANNEL_X, 50, int(round(dutyX)))
-        self.pwm.set_pwm(CHANNEL_Y, 50, int(round(dutyY)))
+        self.pwm.set_pwm(CHANNEL_X, 0, int(round(dutyX)))
+        self.pwm.set_pwm(CHANNEL_Y, 0, int(round(dutyY)))
         self.duties = { 'x': int(round(dutyX)), 'y': int(round(dutyY))}
 
     def directionalMove(self, direction):
